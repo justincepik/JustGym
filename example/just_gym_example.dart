f0e06292dfbd5467a8112b6af1de74d065433f51
Justin Cepik <justin.cepik@semdatex.com>
2022-10-12 23:49:01 +0200

    first commit

---
+++ example/just_gym_example.dart
@@ -0,0 +1,6 @@
+import 'package:just_gym/just_gym.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/just_gym.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library just_gym;
+
+export 'src/just_gym_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
